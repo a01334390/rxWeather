3dd6911f0f2ec87b6970b02efbe03f0a655af244
Fernando Martin Garcia Del Angel <martingarciadelangel@me.com>
2020-02-11 20:29:48 -0600

    Initial Xcode Files

---
+++ rxWeather/rxWeather/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  rxWeather
+//
+//  Created by Fernando Martin Garcia Del Angel on 11/02/20.
+//  Copyright © 2020 Fernando Martin Garcia Del Angel. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
